6b90f4c5462e9a51612ad8005bc9a09decde1446
Sam <sam@sambhattacharyya.com>
2023-12-20 15:07:26 -0600

    Add file size checks

--- src/index.js
+++ src/index.js
@@ -162,13 +162,24 @@ async function setupPreview(data) {
         });
 
         let bitrate = getBitrate();
-        const max_duration = 3500/(bitrate/(8*1024*1024));
+        const max_duration = 2000/(bitrate/(8*1024*1024));
 
         if(video.duration > max_duration){
             Alpine.store('target', 'writer');
         } else {
             Alpine.store('target', 'blob');
         }
+
+        const estimated_size = (bitrate/8)*video.duration + (128/8)*video.duration; // Assume 128 kbps audio
+
+        const quota = (await navigator.storage.estimate()).quota;
+
+        if(estimated_size > quota){
+            return showError(`The video is too big. It would output a file of ${humanFileSize(estimated_size)} but the browser can only write files up to ${humanFileSize(quota)}`);
+        }
+
+
+        Alpine.store('size', humanFileSize(estimated_size))
 
 
         function canvasFullScreen(){
@@ -203,7 +214,7 @@ async function initRecording(){
 
     let bitrate = getBitrate();
 
-    const max_duration = 3500/(bitrate/(8*1024*1024));
+    const max_duration = 2000/(bitrate/(8*1024*1024));
 
     let writer;
 
@@ -262,7 +273,7 @@ async function initRecording(){
             callback(frame);
         },
         error(e) {
-            console.log(e);
+            showError(e.message);
         }
     });
 
@@ -272,11 +283,17 @@ async function initRecording(){
     const encoder = new VideoEncoder({
         output: (chunk, meta) => {
             const callback = encode_callbacks.shift();
-            muxer.addVideoChunk(chunk, meta);
+
+            try {
+                muxer.addVideoChunk(chunk, meta);
+            } catch (e) {
+                showError(e.message);
+            }
+
             callback();
         },
         error: (e) => {
-            console.log(e);
+            showError(e.message);
         }
     });
 
@@ -386,42 +403,82 @@ async function initRecording(){
 
     clearInterval(flush_check);
 
-    const audioData  = await getMP4Data(data, 'audio');
 
-    const source_audio_chunks = audioData.encoded_chunks;
+    try{
+        const audioData  = await getMP4Data(data, 'audio');
+
+        const source_audio_chunks = audioData.encoded_chunks;
 
 
-    for (let audio_chunk of source_audio_chunks){
-        muxer.addAudioChunk(audio_chunk);
+        for (let audio_chunk of source_audio_chunks){
+            muxer.addAudioChunk(audio_chunk);
+        }
+
+
+        Alpine.store('progress', 100);
+
+
+        muxer.finalize();
+
+
+        if(writer){
+            await writer.close();
+        } else{
+            const blob = new Blob([muxer.target.buffer], {type: "video/mp4"});
+            Alpine.store('download_url', window.URL.createObjectURL(blob));
+        }
+
+        Alpine.store('state', 'complete');
+
+
+
+    } catch (e) {
+
+        showError(e.message);
+
     }
 
-
-
-    Alpine.store('progress', 100);
-
-    finished = true;
-
-    muxer.finalize();
-
-
-    if(writer){
-        await writer.close();
-    } else{
-        const blob = new Blob([muxer.target.buffer], {type: "video/mp4"});
-        Alpine.store('download_url', window.URL.createObjectURL(blob));
-    }
-
-    Alpine.store('state', 'complete');
 
 
 
 
 }
+
+function showError(message){
+    Alpine.store('state', 'error');
+    Alpine.store('error', message);
+
+}
+
 
 function getBitrate() {
 
     return 1e7 * (video.videoWidth*video.videoHeight*4)/(1280*720);
 }
+
+function humanFileSize(bytes, si=false, dp=1) {
+    const thresh = si ? 1000 : 1024;
+
+    if (Math.abs(bytes) < thresh) {
+        return bytes + ' B';
+    }
+
+    const units = si
+        ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+        : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+    let u = -1;
+    const r = 10**dp;
+
+    do {
+        bytes /= thresh;
+        ++u;
+    } while (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1);
+
+
+    return bytes.toFixed(dp) + ' ' + units[u];
+}
+
+
 
 function prettyTime(secs){
     var sec_num = parseInt(secs, 10)
